1d6598a723037408afa93d6eb61b6024d975ef35
Peter Mickle <peter.mickle26@d155.org>
2024-09-18 07:28:46 -0500

    Initial Commit

---
+++ gradesProjectMickleTests/gradesProjectMickleTests.swift
@@ -0,0 +1,17 @@
+//
+//  gradesProjectMickleTests.swift
+//  gradesProjectMickleTests
+//
+//  Created by PETER MICKLE on 9/18/24.
+//
+
+import Testing
+@testable import gradesProjectMickle
+
+struct gradesProjectMickleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
